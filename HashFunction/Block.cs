bc85884bfdbc0e7ccae1b2ee0f23cea5bb0539cc
Benas Pučkus <benas.puck@gmail.com>
2019-11-10 17:41:04 +0200

    added blockchain

---
+++ HashFunction/Block.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashFunction
+{
+    public class Block
+    {
+        public int Index { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+        public string Data { get; set; }
+        public int Nonce { get; set; } = 0;
+
+        public Block(DateTime timeStamp, string previousHash, string data)
+        {
+            Index = 0;
+            TimeStamp = timeStamp;
+            PreviousHash = previousHash;
+            Data = data;
+            Hash = CalculateHash();
+        }
+
+        public string CalculateHash()
+        {
+            var data = $"{TimeStamp.ToString()}{PreviousHash}{Data}{Nonce}";
+            var hash = HashFunction.GetHash(data);
+
+            return hash;
+        }
+
+        public void Mine(int difficulty)
+        {
+            var leadingZeros = new string('0', difficulty);
+            while (this.Hash == null || this.Hash.Substring(0, difficulty) != leadingZeros)
+            {
+                this.Nonce++;
+                this.Hash = this.CalculateHash();
+            }
+        }
+    }
+}
